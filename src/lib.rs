9dc1c94c782688c0b143ec41bb80c2cf0c78c5b6
JosephCatrambone <jo.jcat@gmail.com>
2023-05-11 23:15:10 -0700

    Just remove a bunch of the DB stuff.  Tired of fighting with it.  I'll come back later.

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,6 @@ use axum::{
 	Json,
 	Router,
 };
-use sea_orm::{ConnectOptions, ConnectionTrait, Database, DatabaseConnection, DbErr, DbBackend, Statement};
 use serde::{Deserialize, Serialize};
 use sqlx;
 use std::future::IntoFuture;
@@ -32,44 +31,31 @@ const MAX_POOLED_CONNECTIONS: u32 = 5;
 #[derive(Clone, Debug)]
 struct AppState {
 	// templates, cache, etc.
-	db_pool: Arc<DatabaseConnection>,
+	db_pool: Arc<sqlx::Pool<sqlx::Postgres>>,
 }
 
 
-pub async fn build_database_pool(db_connection_str: String) -> AHResult<DatabaseConnection> {
+pub async fn build_database_pool(db_connection_str: String) -> AHResult<sqlx::Pool<sqlx::Postgres>> {
 	// setup connection pool
 	// let db: DatabaseConnection = Database::connect("protocol://username:password@host/database").await?;
-	let db_connection_options = ConnectOptions::new(db_connection_str)
-		.min_connections(5)
-		.max_connections(10)
-		.connect_timeout(Duration::from_secs(5))
-		.idle_timeout(Duration::from_secs(15))
-		.max_lifetime(Duration::from_secs(60))
-		.sqlx_logging(true)
-		.sqlx_logging_level(log::LevelFilter::Debug);
-	let mut db: DatabaseConnection = Database::connect(*db_connection_options).await?;
-	match db.get_database_backend() {
-		DbBackend::Postgres => {
-			println!("Using POSTGRES connection.");
-		}
-		DbBackend::Sqlite => {
-			println!("Using SQLite connection.");
-		}
-		_ => {
-			println!("Unrecognized DB backend.");
-		}
-	}
-
-	Ok(db)
+	
+	let pool = sqlx::postgres::PgPoolOptions::new()
+		.max_connections(MAX_POOLED_CONNECTIONS)
+		//.connect_timeout(Duration::from_secs(3))
+		.connect(&db_connection_str)
+		.await
+		.expect("Can't connect to database");
+	
+	Ok(pool)
 }
 
 
-pub async fn build_main_router(db: DatabaseConnection) -> Router {
+pub async fn build_main_router(db_pool: sqlx::Pool<sqlx::Postgres>) -> Router {
 	// initialize tracing
 	//tracing_subscriber::fmt::init();
 	
 	let app_state = AppState {
-		db_pool: Arc::new(db),
+		db_pool: Arc::new(db_pool),
 	};
 	
 	let routes = Router::new()
@@ -77,7 +63,6 @@ pub async fn build_main_router(db: DatabaseConnection) -> Router {
 		.route("/with_async_fun", get(good_fun))
 		.route("/with_query_args", get(with_arguments))
 		.route("/with_path_args/:name", get(with_path_arguments))
-		//.route("/database", get(using_connection_pool_extractor).post(using_connection_extractor))
 		.with_state(app_state)
 		//.merge(routes_hello())
 	;
@@ -92,51 +77,6 @@ where
 	(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
 
-// Section - Database Pooling
-/*
-
-struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
-
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-	DatabaseConnection: FromRef<S>,
-	S: Send + Sync,
-{
-	type Rejection = (StatusCode, String);
-	
-	async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-		let pool = PgPool::from_ref(state);
-	
-		let conn = pool.acquire().await.map_err(internal_error)?;
-	
-		Ok(Self(conn))
-	}
-}
-*/
-// Section END - Database Pooling
-
-// we can extract the connection pool with `State`
-/*
-async fn using_connection_pool_extractor(
-	state: AppState,
-) -> Result<String, (StatusCode, String)> {
-	sqlx::query_scalar("select 'hello world from pg'")
-		.fetch_one(&pool)
-		.await
-		.map_err(internal_error)
-}
-
-async fn using_connection_extractor(
-	DatabaseConnection(conn): DatabaseConnection,
-) -> Result<String, (StatusCode, String)> {
-	let mut conn = conn;
-	sqlx::query_scalar("select 'hello world from pg'")
-		.fetch_one(&mut conn)
-		.await
-		.map_err(internal_error)
-}
-*/
 
 async fn good_fun() -> impl IntoResponse {
 	Html("What's up again!?")
--- start_dev.sh
+++ start_dev.sh
@@ -1,2 +1,3 @@
 # Watch (quiet) (clear) (only source) (then execute run on changes)
-cargo watch -q -c -w src/ -x run & cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
+#cargo watch -q -c -w src/ -x run & cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
+export DATABASE_URL = "postgresql://c9artuser:c9artpassword@locahost:5432/c9art"
